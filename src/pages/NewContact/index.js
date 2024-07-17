cb2f8c47c1e7f894c773eabafed37ca02838359f
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2024-07-16 22:48:55 -0300

    feat: select component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,17 @@
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
       <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
     </>
   );
 }
