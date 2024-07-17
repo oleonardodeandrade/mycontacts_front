40648e2d41bcd564e4f6a0733b48870494dd6fac
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2024-07-16 22:58:50 -0300

    feat: button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -12,6 +13,12 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="submit">
+        Salvar alterações
+      </Button>
+      <Button type="submit" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
