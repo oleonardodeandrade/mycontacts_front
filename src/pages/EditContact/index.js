fce78572d37afbd978fe081e9ec43042b83fdd5a
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2024-07-18 22:47:53 -0300

    feat: FormGroup

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,5 +1,11 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function EditContact() {
-  return (<PageHeader title="Editar Leonardo Andrade" />);
+  return (
+    <>
+      <PageHeader title="Editar Leonardo Andrade" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
+  );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,24 +1,11 @@
-import Button from '../../components/Button';
-import Input from '../../components/Input';
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
-import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
-      <Input placeholder="Nome" />
-      <Select>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-      </Select>
-      <Button type="submit">
-        Salvar alterações
-      </Button>
-      <Button type="submit" disabled>
-        Salvar alterações
-      </Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
