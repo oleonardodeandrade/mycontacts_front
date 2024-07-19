311ddff9bcc5d52a22d2f773f3b9e7dd7b366ab9
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2024-07-18 22:51:58 -0300

    refactor: Link component Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {
   Card, Container, Header, InputSearchContainer, ListContainer,
 } from './styles';
@@ -14,7 +15,7 @@ export default function Home() {
       </InputSearchContainer>
       <Header>
         <strong>3 Contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -36,52 +37,15 @@ export default function Home() {
           </div>
 
           <div className="actions">
-            <a href="/">
+            <Link to="/edit/123">
               <img src={edit} alt="Edit" />
-            </a>
+            </Link>
             <button type="button">
               <img src={trash} alt="Delete" />
             </button>
           </div>
         </Card>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Leonardo Andrade</strong>
-              <small>Instagram</small>
-            </div>
-            <span>leonardoh.deandrade@gmail.com</span>
-            <span>(17) 99274-4594</span>
-          </div>
 
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Leonardo Andrade</strong>
-              <small>Instagram</small>
-            </div>
-            <span>leonardoh.deandrade@gmail.com</span>
-            <span>(17) 99274-4594</span>
-          </div>
-
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
       </ListContainer>
     </Container>
   );
